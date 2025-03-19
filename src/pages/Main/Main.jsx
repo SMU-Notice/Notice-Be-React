bdb9018113b19479593f1563a2c2733675216128
Parkww214 <pww5148@gmail.com>
2025-03-19 15:44:11 +0900

    docs

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -2,14 +2,22 @@ import { useNavigate } from "react-router-dom";
 
 const Main = () => {
   const navigate = useNavigate();
-  const goTo = () => {
+  const goToMain = () => {
+    navigate('/Main');
+  };
+  const goToSignUp = () => {
+    navigate('/SignUp');
+  };
+  const goToLogin = () => {
     navigate('/Login');
   };
 
   return (
     <div>
-      <h1>Main Page</h1>
-      <button onClick={goTo}>로그인인 페이지로 이동</button>
+      
+      <button onClick={goToMain}>SMU NOTICE</button>
+      <button onClick={goToSignUp}>로그인인 페이지로 이동</button>
+      <button onClick={goToLogin}>로그인인 페이지로 이동</button>
     </div>
   );
   };
